c62da4212bc798862eb0474b8bdb9c2528c428a1
Michael Bartido <jamberjong@gmail.com>
2017-10-25 15:54:09 -0700

    Added conditional button pressing for search bar

--- app/src/App.vue
+++ app/src/App.vue
@@ -10,45 +10,53 @@
     <h2>{{ serverdata }}</h2>
       <v-content>
         <v-container fluid>
-
+            <!--Recipe and Ingredient Buttons-->
             <h2 class="text-xs-center">Search by</h2>
-            
             <v-container grid-list-md text-xs-center>
               <v-layout row justify-center>
                   <v-card-text>
                     <div>
-                      <v-btn color="primary" dark>Recipe</v-btn>
+                      <v-btn color="primary" dark v-on:click="buttonClicked = true; lastClicked = 'Recipe'">Recipe</v-btn>
                     </div>
                   </v-card-text>
 
                   <v-card-text>
                     <div>
-                      <v-btn color="primary" dark>Ingredients</v-btn>
+                      <v-btn color="primary" dark v-on:click= "buttonClicked = true; lastClicked = 'Ingredients'">Ingredients</v-btn>
                     </div>
                   </v-card-text>
                 </v-card>
               </v-layout>
             </v-container>
 
-            <v-card-text>
-              <v-container fluid>
-                <v-layout row>
-                  <v-flex xs4>
-                    <v-subheader>Search</v-subheader>
-                  </v-flex>
-                  <v-flex xs8>
-                    <v-text-field
-                      label="Recipe/Ingredients"
-                      v-model="input">
-                    </v-text-field>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-            </v-card-text>
-            <p transition='expand'>Input is: {{input}}</p>
+            <!--Search Bar area-->
+            <v-container fluid v-if="buttonClicked">
+              <v-card-text>
+                <v-container fluid>
+                  <v-layout row>
+                    <v-flex xs4>
+                      <v-subheader>Search</v-subheader>
+                    </v-flex>
+                    <v-flex xs8>
+                      
+                      <v-text-field v-if="lastClicked === 'Recipe'"
+                        label="Recipe"
+                        v-model="input">
+                      </v-text-field>
 
-          
+                       <v-text-field v-if="lastClicked == 'Ingredients'"
+                        label="Ingredients"
+                        v-model="input">
+                      </v-text-field>
+                    
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card-text>
+              <p>Input is {{input}}</p>
+            </v-container>  
         </v-container>
+
       </v-content>
     </main>
     
@@ -60,27 +68,28 @@
 
 <script>
   import axios from 'axios';
-
+  
   export default {
     data () {
       return {
         serverdata: '',
         title: 'Recipr',
-        input: ''
+        input: '',
+        buttonClicked: false,
+        lastClicked: ''
       }
     },
+
 
     mounted() {
       axios.get('http://localhost:8081/api')
         .then((res) => {
           console.log(res.data);
-          this.serverdata = res.data;
+          this.serverdata = input;
         })
         .catch((error) => {
           console.log(error);
         });
+      }
     }
-
-  
-  }
 </script>
