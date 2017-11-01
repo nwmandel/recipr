5506662725682b05e45e42348d568a93aecc3a62
Nicholas Mandel <nwmandel@gmail.com>
2017-10-31 19:58:57 -0700

    parsed name link and image of recipes

--- parset.js
+++ parset.js
@@ -1,7 +1,7 @@
 //var input = require('./out1.json');
 var axios = require('axios');
 const mkey = require('./mashapekey.json');
-var out = require('./out2.json');
+var out = require('./out3.json');
   
 var config = {
     headers: {
@@ -10,23 +10,25 @@ var config = {
     }
  };
 
-function getSiteRecipe_ax(query, config) {
-	var result = "";
-	axios.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/site/search?query="+query, config)
-		.then(res => {return res.data});
-	}
-
 var parsej = (input) => {
-	var obj = JSON.parse(input, (key, val) => {
-		if (key == "Recipes") {
-			console.log("found recipies");
-		} else {
-			console.log("didnt find recipies" + val);
+	var input_ = JSON.stringify(input);
+	var parsing = JSON.parse(input_);
+	var keys = Object.keys(parsing);
+	for (let i = 0; i < keys.length; i++) {
+		var key = keys[i];
+		var val = parsing[key];
+		if (key === "Recipes") {
+
+			// loops over each recipe in json 
+			for (let j = 0; j < val.length; j++) {
+				console.log(val[j].name);	// returns name
+				console.log(val[j].link);	// returns link
+				console.log(val[j].image);	// returns image link
+			}
 		}
-	});
+	}
 }
 
+parsej2(out);
 
-//var api = getSiteRecipe_ax("tomato", config);
-//console.log(api);
-parsej(out);
+
