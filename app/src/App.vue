ab837a3dde132508b6b65f345cf24799e039fa32
Michael Bartido <jamberjong@gmail.com>
2017-11-01 16:22:25 -0700

    Added api call to tables

--- app/src/App.vue
+++ app/src/App.vue
@@ -241,91 +241,7 @@
             calcium: '6%',
             iron: '7%',
             image: 'https://d30y9cdsu7xlg0.cloudfront.net/png/9711-200.png'
-          },
-          {
-            value: false,
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            sodium: 413,
-            calcium: '3%',
-            iron: '8%',
-            image: 'https://d30y9cdsu7xlg0.cloudfront.net/png/9711-200.png'
-          },
-          {
-            value: false,
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            sodium: 327,
-            calcium: '7%',
-            iron: '16%',
-            image: 'https://d30y9cdsu7xlg0.cloudfront.net/png/9711-200.png'
-          },
-          {
-            value: false,
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            sodium: 50,
-            calcium: '0%',
-            iron: '0%',
-            image: 'https://d30y9cdsu7xlg0.cloudfront.net/png/9711-200.png'
-          },
-          {
-            value: false,
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            sodium: 38,
-            calcium: '0%',
-            iron: '2%',
-            image: 'https://d30y9cdsu7xlg0.cloudfront.net/png/9711-200.png'
-          },
-          {
-            value: false,
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            sodium: 562,
-            calcium: '0%',
-            iron: '45%',
-            image: 'https://d30y9cdsu7xlg0.cloudfront.net/png/9711-200.png'
-          },
-          {
-            value: false,
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            sodium: 326,
-            calcium: '2%',
-            iron: '22%',
-            image: 'https://d30y9cdsu7xlg0.cloudfront.net/png/9711-200.png'
-          },
-          {
-            value: false,
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            sodium: 54,
-            calcium: '12%',
-            iron: '6%',
-            image: 'https://d30y9cdsu7xlg0.cloudfront.net/png/9711-200.png'
-          }
+          } 
         ]
       }
     },
@@ -371,11 +287,24 @@
           if (key === "Recipes") {
             console.log(key,val);
 
+            
             // loops over each recipe in json 
             for (let j = 0; j < val.length; j++) {
               this.pres += val[j].name;   // returns name
               this.pres += val[j].link;   // returns link
               this.imlink.push(val[j].image);  // adds to array of image links
+              this.items.push({
+                value: false,
+                name: val[j].name,
+                calories: 9000,
+                fat: '9000',
+                carbs: 9000,
+                protein: 9000,
+                sodium: 9000,
+                calcium: '9000%',
+                iron: '9000%',
+                image: val[j].image
+              });
             }
           }
         }
